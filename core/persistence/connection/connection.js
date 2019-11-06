1ed767f0c29b2da0cbb6c84385ef3adf58b17898
David Conde <conde@MacBook-Pro-de-David.local>
2019-11-06 12:15:07 -0700

    add mysql connection

--- core/persistence/connection/connection.js
+++ core/persistence/connection/connection.js
@@ -1,14 +1,16 @@
 const config = require("../../../config");
 const mongoose = require("mongoose");
+const mysql = require('mysql');
 const logger = require("../../../utils/logger");
 
+ // MONGODB
 mongoose.connect(config.mongodb.defaultconnection,{
     reconnectTries: Number.MAX_VALUE,
     reconnectInterval: 500,
     poolSize: 10
 });
 
-mongoose.connection.on("connected", ()=>{
+mongoose.connection.on("connected", ()=>{ 
     logger.info(`Mongoose connected to: ${config.mongodb.defaultconnection}`);
 });
 
@@ -19,3 +21,18 @@ mongoose.connection.on("error", (err)=>{
 mongoose.connection.on("disconnected", ()=>{
     logger.info(`Mongoose disconnected`);
 });
+// MYSQL 
+
+const connection_mysql = mysql.createConnection({
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database
+});
+connection_mysql.connect((err) => {
+    if (err) {
+        throw err;
+    } 
+    logger.info('Connected to database mysq');
+});
+global.connection_mysql = connection_mysql;
