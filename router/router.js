0593f25c3f2c584f5862b08b0e1c06e06e92e798
David Conde <conde@MacBook-Pro-de-David.local>
2019-11-10 01:11:30 -0700

    ya alv

--- router/router.js
+++ router/router.js
@@ -13,18 +13,18 @@ router.post("/api/login", AuthController.login);
 
 // USUARIOS
 router.post("/api/user", usuarioController.postUsuario); 
-router.get("/api/user/:idUsuario", usuarioController.getUsuario);
-router.put("/api/user/:idUsuario", usuarioController.putUsuario);
-router.delete("/api/user/:idUsuario", usuarioController.deleteUsuario); 
-router.get("/api/users", usuarioController.getUsuarios); 
+router.get("/api/user/:idUsuario", middlewares.tokenMiddleware, usuarioController.getUsuario);
+router.put("/api/user/:idUsuario", middlewares.tokenMiddleware, usuarioController.putUsuario);
+router.delete("/api/user/:idUsuario", middlewares.tokenMiddleware, usuarioController.deleteUsuario); 
+router.get("/api/users", middlewares.tokenMiddleware, usuarioController.getUsuarios); 
 
 // CONOCIDOS
 router.post("/api/conocido/:idUsuario", conocidoController.postConocido);
 
 // POSTS
 router.post("/api/post", middlewares.tokenMiddleware, postController.postPost);
-router.get("/api/posts/tags/:tag", postController.getPostsFind);
-router.get("/api/post/:postid", postController.getPostsID);
+router.get("/api/posts/tags/:tag", middlewares.tokenMiddleware, postController.getPostsFind);
+router.get("/api/post/:postid", middlewares.tokenMiddleware, postController.getPostsID);
 router.get("/api/posts", middlewares.tokenMiddleware, postController.getPosts);
 router.get("/api/posts/:idUsuario", middlewares.tokenMiddleware, postController.getPostsUser);
 router.delete("/api/post/:postid", middlewares.tokenMiddleware, postController.deletePost); 
