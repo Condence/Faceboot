3fe59731a1ad94c88408226cc2f58aa9a18ac765
ikkix16 <marioxbarreras@gmail.com>
2019-11-25 12:15:20 -0800

    Base

---
+++ main.js
@@ -0,0 +1,61 @@
+const app= new Vue({
+    el:"#app",
+    data:{
+
+    },
+
+    methods:{
+
+        //Usuario
+        agregarUsuario(){
+            
+        },
+        obtenerUsuarios(){
+
+        },
+        obtenerUsuarioID(){
+
+        },
+
+        actualizarUsuario(){
+
+        },
+
+        borrarUsuarioID(){
+
+            //deleteUsuario
+
+        },
+
+        //post
+        agregarPost(){
+
+        },
+
+        obtenerPost(){
+
+        },
+
+        obtenerPostIdUser(){
+
+        },
+        //Conocido
+
+        agregarConocido(){
+
+        },
+
+        iniciarSesion(){
+
+        }
+
+
+    },
+
+  
+
+    template: {
+
+    }
+    
+});
